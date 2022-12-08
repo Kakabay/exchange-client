f6be3f040bbfb328f0f4d053966d261973e58045
Kakabay <kashyrberdyew@gmail.com>
2022-12-08 16:34:08 +0500

    Tarif page done

--- src/components/TarifService.jsx
+++ src/components/TarifService.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-const TarifService = ({ title, price, activeTab }) => {
+const TarifService = ({ title, price }) => {
   return (
     <div className="tarif-service-item">
       <h2 className="tarif-service-text">{title}</h2>
-      <h2 className="tarif-service-price">
-        {price}{' '}
-        {activeTab === 1 ? (price.length < 7 ? 'TMT' : null) : price.length < 7 ? '$' : null}
-      </h2>
+      <h2 className="tarif-service-price">{price} </h2>
     </div>
   );
 };
--- src/pages/Tarif.jsx
+++ src/pages/Tarif.jsx
@@ -37,78 +37,30 @@ const Tarif = () => {
           </div>
           {activeTab === 1 ? (
             <div className="tarif-table">
-              <TarifService
-                title="Составление, распечатка и оформление проектов и экземпляров контрактов (с учетом стоимости
-              бумаги, за одну страницу), информация на одной странице"
-                price="100"
-                activeTab={activeTab}
-              />
-              <TarifService
-                title="Брокерские услуги"
-                price="0 – 525 000 - 525 ТМТ Свыше 525 001 ТМТ - 0.1 %"
-                activeTab={activeTab}
-              />
-              <TarifService title="Покупка брокерского места" price="10 000" />
-              <TarifService
-                title="Регистрация дополнительных соглашений (Одно дополнительное соглашение независимо от количества страниц)"
-                price="350"
-                activeTab={activeTab}
-              />
-              <TarifService
-                title="Информация по ценам (за одну позицию)"
-                price="105"
-                activeTab={activeTab}
-              />
-              <TarifService
-                title="Регистрация клиента отделом финансового мониторинга"
-                price="525"
-                activeTab={activeTab}
-              />
-              <TarifService
-                title="Сведения о ценах, в том, числе заключение по цене (стоимость одной позиции, но общая стоимость не должна превышать стоимость 100 позиций)"
-                price="105"
-                activeTab={activeTab}
-              />
+              {tarifData
+                ? tarifData.data.map((tarif) => {
+                    return tarif.type === 'resident' ? (
+                      <TarifService
+                        title={tarif.title}
+                        price={tarif.prices.map((tarifPrice) => tarifPrice.price + ' ')}
+                      />
+                    ) : null;
+                  })
+                : null}
             </div>
           ) : null}
           {activeTab === 2 ? (
             <div className="tarif-table">
-              <TarifService
-                title="Составление, распечатка и оформление проектов и экземпляров контрактов (с учетом стоимости
-              бумаги, за одну страницу), информация на одной странице"
-                price="100"
-                activeTab={activeTab}
-              />
-              <TarifService title="Брокерские услуги" price="150" />
-              <TarifService
-                title="Покупка брокерского места"
-                price="$5 000
-                $10 000 (для покупателей товарной
-                продукции нефти-газовой
-                отрасли)
-                "
-                activeTab={activeTab}
-              />
-              <TarifService
-                title="Регистрация дополнительных соглашений (Одно дополнительное соглашение независимо от количества страниц)"
-                price="100"
-                activeTab={activeTab}
-              />
-              <TarifService
-                title="Информация по ценам (за одну позицию)"
-                price="30"
-                activeTab={activeTab}
-              />
-              <TarifService
-                title="Регистрация клиента отделом финансового мониторинга"
-                price="150"
-                activeTab={activeTab}
-              />
-              <TarifService
-                title="Сведения о ценах, в том, числе заключение по цене (стоимость одной позиции, но общая стоимость не должна превышать стоимость 100 позиций)"
-                price="30"
-                activeTab={activeTab}
-              />
+              {tarifData
+                ? tarifData.data.map((tarif) => {
+                    return tarif.type === 'non_resident' ? (
+                      <TarifService
+                        title={tarif.title}
+                        price={tarif.prices.map((tarifPrice) => tarifPrice.price + ' ')}
+                      />
+                    ) : null;
+                  })
+                : null}
             </div>
           ) : null}
         </div>
