42f5a247339eed65b37947d9e2cfba2890789145
Kakabay <kashyrberdyew@gmail.com>
2022-12-08 15:00:15 +0500

    Contacts page updated

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,18 +1,18 @@
 // Modules
-import { useState, useEffect } from "react";
-import { Api } from "../helpers/api";
+import { useState, useEffect } from 'react';
+import { Api } from '../helpers/api';
 // Components
-import SectionTitle from "../components/SectionTitle";
-import ContactsCard from "../components/ContactsCard";
+import SectionTitle from '../components/SectionTitle';
+import ContactsCard from '../components/ContactsCard';
 
 const Contacts = () => {
   const [contactsData, setContactsData] = useState();
 
   useEffect(() => {
     const ContactsApi = new Api(
-      "http://tmex.gov.tm:8765/api/contacts",
+      'http://tmex.gov.tm:8765/api/contacts',
       contactsData,
-      setContactsData
+      setContactsData,
     );
     ContactsApi.get();
 
@@ -38,85 +38,15 @@ const Contacts = () => {
                       firstFax={contactCard.contacts[0].fax}
                       firstMail={contactCard.contacts[0].mail}
                       secondPosition={
-                        contactCard.contacts[1]?.title
-                          ? contactCard.contacts[1]?.title
-                          : null
+                        contactCard.contacts[1]?.title ? contactCard.contacts[1]?.title : null
                       }
                       secondPhone={
-                        contactCard.contacts[1]?.phone
-                          ? contactCard.contacts[1]?.phone
-                          : null
+                        contactCard.contacts[1]?.phone ? contactCard.contacts[1]?.phone : null
                       }
                     />
                   );
                 })
               : null}
-            {/* <ContactsCard
-              title="Председатель"
-              firstPosition="Приемная"
-              firstPhone="+993 (12) 446630"
-              firstFax="+993 (12) 446616"
-              firstMail="arza-shikayat@exchange.gov.tm"
-            />
-            <ContactsCard
-              title="Заместитель председателя"
-              firstPosition="Приемная"
-              firstPhone="+993 (12) 446101"
-            />
-            <ContactsCard
-              title="Отдел организации биржевой торговли"
-              firstPosition="Начальник отдела"
-              firstPhone="+993 (12) 446116"
-              secondPosition="Маклер"
-              secondPhone="+993 (12) 446275"
-            />
-            <ContactsCard
-              title="Департамент анализа и новых технологий"
-              firstPosition="Начальник отдела"
-              firstPhone="+993 (12) 446020"
-              secondPosition="Зам. нач. отдела"
-              secondPhone="+993 (12) 446015"
-            />
-            <ContactsCard
-              title="Отдел регистрации биржевых контрактов (договоров)"
-              firstPosition="Начальник отдела"
-              firstPhone="+993 (12) 446030"
-              secondPosition="Зам. нач. отдела"
-              secondPhone="+993 (12) 446260"
-            />
-            <ContactsCard
-              title="Отдел анализа цен и рынка"
-              firstPosition="Начальник отдела"
-              firstPhone="+993 (12) 446055"
-            />
-            <ContactsCard
-              title="Бухгалтерия"
-              firstPosition="Начальник отдела"
-              firstPhone="+993 (12) 446070"
-              secondPosition="Зам. нач. отдела"
-              secondPhone="+993 (12) 446071"
-            />
-            <ContactsCard
-              title="Отдел взаиморасчетов"
-              firstPosition="Начальник отдела"
-              firstPhone="+993 (12) 446080"
-              secondPosition="Зам. нач. отдела"
-              secondPhone="+993 (12) 446037"
-            />
-            <ContactsCard
-              title="Отдел экономического анализа"
-              firstPosition="Начальник отдела"
-              firstPhone="+993 (12) 446090"
-              secondPosition="Зам. нач. отдела"
-              secondPhone="+993 (12) 446091"
-            />
-            <ContactsCard
-              title="Юридический отдел"
-              firstPosition="Начальник отдела"
-              firstPhone="+993 (12) 446040"
-              secondPosition="Зам. нач. отдела"
-              secondPhone="+993 (12) 446047"
-            /> */}
           </div>
         </div>
       </div>
