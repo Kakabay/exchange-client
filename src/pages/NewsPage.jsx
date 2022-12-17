2c7ce0b1fd2b94cb13298fea872c7e78af86955b
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-12-18 03:08:04 +0500

    news page pagination added

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -6,18 +6,24 @@ import { dateReverse } from "../helpers/functions";
 // Components
 import SectionTitle from "../components/SectionTitle";
 import NewsPost from "../components/NewsPost";
+// Icons
+import { ReactComponent as Arrow } from "../assets/icons/arrow.svg";
 
 const NewsPage = ({ lang, setPostId, postsData, setPostsData }) => {
+  const [url, setUrl] = useState("http://tmex.gov.tm:8765/api/news");
+
   useEffect(() => {
     // News fetch
-    const PostsData = new Api(
-      "http://tmex.gov.tm:8765/api/news",
-      postsData,
-      setPostsData
-    ).get({
+    const PostsData = new Api(url, postsData, setPostsData).get({
       "X-Localization": lang,
     });
-  }, [lang]);
+  }, [lang, url]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 300);
+  }, [url]);
 
   return (
     <main>
@@ -42,6 +48,41 @@ const NewsPage = ({ lang, setPostId, postsData, setPostsData }) => {
                 })
               : null}
           </div>
+          {postsData ? (
+            <div className="pagination">
+              <button
+                disabled={!postsData.meta.pagination.links.previous}
+                className={"arr-left pagination-arrow"}
+                onClick={() => {
+                  postsData
+                    ? postsData.meta.pagination.links.previous
+                      ? setUrl(postsData.meta.pagination.links.previous)
+                      : null
+                    : null;
+                }}
+              >
+                <Arrow className="arr-pagination-svg" />
+              </button>
+              <div className="pagination-count">
+                {postsData
+                  ? `${postsData.meta.pagination.current_page} / ${postsData.meta.pagination.total_pages}`
+                  : "1 / 1"}
+              </div>
+              <button
+                disabled={!postsData.meta.pagination.links.next}
+                className={"arr-right pagination-arrow"}
+                onClick={() => {
+                  postsData
+                    ? postsData.meta.pagination.links.next
+                      ? setUrl(postsData.meta.pagination.links.next)
+                      : null
+                    : null;
+                }}
+              >
+                <Arrow className="arr-pagination-svg" />
+              </button>
+            </div>
+          ) : null}
         </div>
       </div>
     </main>
