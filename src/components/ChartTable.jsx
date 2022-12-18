0f82e0c739b0490433efec7a131b8b4c17408527
Kakabay <kashyrberdyew@gmail.com>
2022-12-18 16:52:11 +0500

    Chart table langs added

--- src/components/ChartTable.jsx
+++ src/components/ChartTable.jsx
@@ -1,10 +1,10 @@
 // Modules
-import { useState, useEffect } from "react";
-import { Api } from "../helpers/api";
+import { useState, useEffect } from 'react';
+import { Api } from '../helpers/api';
 // Components
-import TableRow from "./TableRow";
+import TableRow from './TableRow';
 
-const ChartTable = ({ activeRow, setActiveRow, tabIndex }) => {
+const ChartTable = ({ activeRow, setActiveRow, tabIndex, lang }) => {
   const [dataTable, setDataTable] = useState();
 
   // Functions
@@ -19,7 +19,7 @@ const ChartTable = ({ activeRow, setActiveRow, tabIndex }) => {
         `http://tmex.gov.tm:8765/api/categories/${tabIndex}/tradings`,
         // `http://tmex.gov.tm:8765/api/categories/8/tradings`,
         dataTable,
-        setDataTable
+        setDataTable,
       ).get();
     }
   }, [tabIndex]);
@@ -27,9 +27,33 @@ const ChartTable = ({ activeRow, setActiveRow, tabIndex }) => {
   return (
     <div className="table">
       <div className="table-top">
-        <h4 className="table-title">Товарная позиция</h4>
-        <h4 className="table-title">Изменение цены</h4>
-        <h4 className="table-title">Текущая цена</h4>
+        <h4 className="table-title">
+          {lang === 'ru'
+            ? 'Товарная позиция'
+            : lang === 'tm'
+            ? 'Harydyň ady'
+            : lang === 'en'
+            ? 'Product name'
+            : null}
+        </h4>
+        <h4 className="table-title">
+          {lang === 'ru'
+            ? 'Изменение цены'
+            : lang === 'tm'
+            ? 'Bahanyň üýtgeýşi'
+            : lang === 'en'
+            ? 'Price change'
+            : null}
+        </h4>
+        <h4 className="table-title">
+          {lang === 'ru'
+            ? 'Текущая цена'
+            : lang === 'tm'
+            ? 'Häzirki baha'
+            : lang === 'en'
+            ? 'Current price'
+            : null}
+        </h4>
       </div>
       <div className="table-bottom">
         {dataTable
@@ -38,12 +62,12 @@ const ChartTable = ({ activeRow, setActiveRow, tabIndex }) => {
                 <TableRow
                   key={tableRow.id}
                   title={tableRow.title}
-                  titleHide={tableRow.title.length > 7 ? "title-hide" : null}
+                  titleHide={tableRow.title.length > 7 ? 'title-hide' : null}
                   priceChange={tableRow.price_change}
                   price={tableRow.price}
                   currency={tableRow.currency}
                   onClick={() => rowActive(index)}
-                  active={activeRow === index ? "active" : ""}
+                  active={activeRow === index ? 'active' : ''}
                 />
               );
             })
