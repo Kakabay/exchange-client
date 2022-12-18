7cad15e22aead09229afe5bef23754be153af57d
Kakabay <kashyrberdyew@gmail.com>
2022-12-18 16:52:35 +0500

    NewsPosts langs added

--- src/components/NewsPost.jsx
+++ src/components/NewsPost.jsx
@@ -1,8 +1,8 @@
 // Icons
-import { Link } from "react-router-dom";
-import arrow from "../assets/icons/arrow.svg";
+import { Link } from 'react-router-dom';
+import arrow from '../assets/icons/arrow.svg';
 
-const NewsPost = ({ title, shortDes, date, image, setPostId, id }) => {
+const NewsPost = ({ title, shortDes, date, image, setPostId, id, lang }) => {
   return (
     <div className="news-post">
       <div className="news-post-img">
@@ -13,13 +13,17 @@ const NewsPost = ({ title, shortDes, date, image, setPostId, id }) => {
         <h2 className="news-post-title">{title}</h2>
         <p className="news-post-text">{shortDes}</p>
       </div>
-      <Link
-        className="news-post-link"
-        to={`/post/${id}`}
-        onClick={() => setPostId(id)}
-      >
+      <Link className="news-post-link" to={`/post/${id}`} onClick={() => setPostId(id)}>
         <img src={arrow} alt="arrow" />
-        <h3>Читать полностью </h3>
+        <h3>
+          {lang === 'ru'
+            ? 'Читать полностью'
+            : lang === 'tm'
+            ? 'Doly okamak'
+            : lang === 'en'
+            ? 'Read all'
+            : null}
+        </h3>
       </Link>
     </div>
   );
