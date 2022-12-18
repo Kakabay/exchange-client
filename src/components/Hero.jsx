4198a5d4e35ed467fc44b17afd170f3bbde28d35
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-12-18 12:36:07 +0500

    hero buttons bugs fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // Modules
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -8,10 +8,14 @@ const Hero = () => {
       <div className="container">
         <div className="hero-wrapper">
           <div className="buttons-wrapper">
-            <a href="https://tmex.gov.tm/tm" target="_blank" className="hero-button">
+            <a
+              href="https://tmex.gov.tm/tm"
+              target="_blank"
+              className="hero-button"
+            >
               Перейти на торговую площадку
             </a>
-            <a href="" className="hero-button">
+            <a href="/" className="hero-button">
               Подать документы онлайн
             </a>
           </div>
