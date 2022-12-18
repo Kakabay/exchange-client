79f0e11daea21849d2cbd959616450d6e5fa912a
Kakabay <kashyrberdyew@gmail.com>
2022-12-18 16:33:16 +0500

    Hero langs added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,30 @@
 // Modules
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Hero = () => {
+const Hero = ({ lang }) => {
   return (
     <section className="hero">
       <div className="container">
         <div className="hero-wrapper">
           <div className="buttons-wrapper">
-            <a
-              href="https://tmex.gov.tm/tm"
-              target="_blank"
-              className="hero-button"
-            >
-              Перейти на торговую площадку
+            <a href={`https://tmex.gov.tm/${lang}`} target="_blank" className="hero-button">
+              {lang === 'ru'
+                ? 'Перейти на торговую площадку'
+                : lang === 'tm'
+                ? 'Söwda meýdança geçmek'
+                : lang === 'en'
+                ? 'Go to the marketplace'
+                : null}
             </a>
             <a href="/" className="hero-button">
-              Подать документы онлайн
+              {lang === 'ru'
+                ? 'Подать документы онлайн'
+                : lang === 'tm'
+                ? 'Resminamalary onlaýn tabşyrmak'
+                : lang === 'en'
+                ? 'Apply documents online'
+                : null}
             </a>
           </div>
         </div>
